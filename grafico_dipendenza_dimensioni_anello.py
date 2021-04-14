72b76d1fd44e18ab5a9d8e375356eb296fd74c55
marcoradaelli <marco.radaelli7@studenti.unimi.it>
2021-04-14 19:09:25 +0200

    Aggiornamento dati

--- grafico_dipendenza_dimensioni_anello.py
+++ grafico_dipendenza_dimensioni_anello.py
@@ -6,8 +6,8 @@ import time
 path = "data/dipendenza_punti_anello.txt"
 dati = data_loader.carica_file(path)
 
-plt.errorbar(dati['numero passi prima di misura'], dati['ave quantistico'], yerr=dati['devstd quantistico'], label="Quantum")
-plt.errorbar(dati['numero passi prima di misura'], dati['ave classico'], yerr=dati['devstd classico'], label="Classical")
+plt.errorbar(dati['numero punti anello'], dati['ave quantistico'], yerr=dati['devstd quantistico'], label="Quantum")
+plt.errorbar(dati['numero punti anello'], dati['ave classico'], yerr=dati['devstd classico'], label="Classical")
 plt.suptitle("Number of steps before measurement vs Shannon Entropy")
 plt.title("Number of MonteCarlo steps: 100")
 plt.xlabel("Number of steps before measurement")
