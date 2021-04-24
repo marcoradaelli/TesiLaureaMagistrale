f0b20e6ef85c605748c65d121c605298695150b6
marcoradaelli <marco.radaelli7@studenti.unimi.it>
2021-04-24 09:47:12 +0200

    Cammino classico con matrice di transizione

--- studio_fourier_mixing.py
+++ studio_fourier_mixing.py
@@ -1,17 +1,21 @@
 import numpy as np
-from walks_core import qrw,anello
+from walks_core import qrw,crw,anello
 from strumenti_analisi import convoluzione as conv
 
 a = anello.anello(25)
-walk = qrw.walker(anello_ospite=a,
-                  posizione_iniziale=1,
-                  moneta_iniziale=np.array([np.cos(np.pi/4), np.sin(np.pi/4)]))
+q_walk = qrw.walker(anello_ospite=a,
+                    posizione_iniziale=1,
+                    moneta_iniziale=np.array([np.cos(np.pi/4), np.sin(np.pi/4)]))
+c_walk = crw.walker(anello_ospite=anello,
+                    posizione_iniziale=1)
 
 for i in range(0,10):
-    walk.passo()
+    q_walk.passo()
 
-distribuzione, appo= walk.ottieni_distribuzione_probabilita()
+distribuzione, appo= q_walk.ottieni_distribuzione_probabilita()
 tdf = conv.trasformata_fourier(distribuzione)
 
 print(distribuzione)
 print(tdf)
+print(conv.diaconis_shashahani_bound(distribuzione,100))
+print(conv.entropy_convolution_bound(distribuzione,100))
